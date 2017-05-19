c5b1b43239bc2fe35affc20973eb4ddc62782f6c
mont <mont@mont-PC>
2017-05-20 00:34:17 +0100

    stabe 1.2

--- src/javagithubtest/JavaGitHubTest.java
+++ src/javagithubtest/JavaGitHubTest.java
@@ -11,7 +11,7 @@ public class JavaGitHubTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("this is a test");
+        System.out.println("this is a  yolllo");
     }
     
 }
