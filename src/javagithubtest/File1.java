e4850cf8ba7228967579dbda732234734c30b9d9
JoaoM <JoaoM@ASUSDESKTOP-PC>
2017-05-20 02:42:05 +0100

    comment test 1

--- src/javagithubtest/File1.java
+++ src/javagithubtest/File1.java
@@ -5,10 +5,6 @@ package javagithubtest;
  */
 public class File1 {
     
-<<<<<<< HEAD
     //wtf!!!!!   test1
-=======
-    //wtf!!!!!  test 2
->>>>>>> branch 'master' of https://github.com/jgvmonteiro/GitHubTest.git
 
 }
